e91bd460833528344bfc5ef66def716025321a45
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2024-04-06 00:51:48 +0300

    LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,11 @@ import { Filter } from './Filter/Filter';
 import { FormContacts } from './Form/Form';
 import { ContactList } from './ContactList/ContactList';
 import { Section } from './Section/Section';
+import { logDOM } from '@testing-library/react';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -50,6 +46,20 @@ export class App extends Component {
     );
     return visibleContacts;
   }
+
+  componentDidMount() {
+    console.log(localStorage.contacts);
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    this.setState({ contacts: parseContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
